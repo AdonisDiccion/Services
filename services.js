ea2b9f03eed6bb55586935456dd6f7e35045a97e
Adonis Diccion <adonisdiccion2408@gmail.com>
2024-01-30 01:12:58 +0800

    first commit

---
+++ services.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,     // Set to true if you want the carousel to auto-play
+        autoplaySpeed: 3000, // Set the autoplay speed in milliseconds
+        dots: true,         // Set to true if you want to display navigation dots
+        arrows: true        // Set to true if you want to display navigation arrows
+    });
+});
